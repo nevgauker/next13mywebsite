2f4f1ad9766a18ea3d0d4cf53ffcd86d3f6e4d22
rotem <nevgauker@gmail.com>
2023-10-12 03:48:50 +0300

    add new files

---
+++ app/components/article_card/ArticleCard.js
@@ -0,0 +1,18 @@
+import Card from "react-bootstrap/Card";
+
+import styles from "./ArticleCard.module.css";
+
+function ArticleCard({ title, subTitle ,link,text}) {
+  return (
+      <Card className={styles.articleCard } style={{ width: '15rem' }}>
+      <Card.Body>
+              <Card.Title>{ title }</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">{ subTitle }</Card.Subtitle>
+              <Card.Text>{text}</Card.Text>
+        <Card.Link href={link} className={styles.articleLink}>Continue reading</Card.Link>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default ArticleCard;
